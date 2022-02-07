4fbfb9d846d864f527167c45666c3ef21f51da78
rodrigoross <rodrigo.ross.comp@gmail.com>
2022-02-07 17:38:48 -0400

    test: verifica se projetos pertence ao dono

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -13,11 +13,26 @@ class ProjectsTest extends TestCase
     use WithFaker, RefreshDatabase;
 
     /** @test */
-    public function only_authenticated_users_can_create_projects()
+    public function guests_cannot_create_projects()
     {
         $attributes = Project::factory()->raw();
 
         $this->post('/projects', $attributes)->assertRedirect('login');
+    }
+
+    /** @test */
+    public function guest_may_not_view_projects()
+    {
+        $this->get('/projects')->assertRedirect('login');
+    }
+
+    /** @test */
+    public function guest_may_not_view_a_single_project()
+    {
+        $project = Project::factory()->create();
+
+
+        $this->get($project->path())->assertRedirect('login');
     }
 
     /** @test */
@@ -60,12 +75,26 @@ class ProjectsTest extends TestCase
     }
 
     /** @test */
-    public function a_user_can_view_a_project()
+    public function a_user_can_view_their_project()
     {
-        $project = Project::factory()->create();
+        $this->be(User::factory()->create());
+
+        $this->withoutExceptionHandling();
+
+        $project = Project::factory()->create(['owner_id' => auth()->id()]);
 
         $this->get($project->path())
             ->assertSee($project->title)
             ->assertSee($project->description);
+    }
+
+    /** @test */
+    public function an_authenticated_user_cannot_view_the_project_of_others()
+    {
+        $this->be(User::factory()->create());
+
+        $project = Project::factory()->create();
+
+        $this->get($project->path())->assertStatus(403);
     }
 }
