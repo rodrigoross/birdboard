3521528866e796b2a6826ca86103f3af29887c74
rodrigoross <rodrigo.ross.comp@gmail.com>
2022-02-07 14:24:40 -0400

    test(requests): valida requests ao criar projetos

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Project;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -21,7 +22,7 @@ class ProjectsTest extends TestCase
         $this->withoutExceptionHandling();
 
         $attributes = [
-            'title' => $this->faker->sentence(3),
+            'title' => $this->faker->sentence(2),
             'description' => $this->faker->paragraph()
         ];
 
@@ -30,5 +31,21 @@ class ProjectsTest extends TestCase
         $this->assertDatabaseHas('projects', $attributes);
 
         $this->get('/projects')->assertSee($attributes['title']);
+    }
+
+    /** @test */
+    public function a_project_require_a_title()
+    {
+        $attributes = Project::factory()->raw(['title' => '']);
+
+        $this->post('/projects', $attributes)->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_project_require_a_description()
+    {
+        $attributes = Project::factory()->raw(['description' => '']);
+
+        $this->post('/projects', $attributes)->assertSessionHasErrors('description');
     }
 }
