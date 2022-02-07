174f69a06829df4295cba883c6f3a47b85c5e0dc
rodrigoross <rodrigo.ross.comp@gmail.com>
2022-02-07 14:09:30 -0400

    test(projects): um usuário pode criar projetos

---
+++ tests/Feature/ProjectsTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProjectsTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+
+    /**
+     * Testa se um usuario pode criar projetos.
+     *
+     * @test
+     * @return void
+     */
+    public function a_user_can_create_a_project()
+    {
+        $this->withoutExceptionHandling();
+
+        $attributes = [
+            'title' => $this->faker->sentence(3),
+            'description' => $this->faker->paragraph()
+        ];
+
+        $this->post('/projects', $attributes)->assertRedirect('/projects');
+
+        $this->assertDatabaseHas('projects', $attributes);
+
+        $this->get('/projects')->assertSee($attributes['title']);
+    }
+}
