cc8db015d66c8e5fe8a1567ee6c100d114966874
rodrigoross <rodrigo.ross.comp@gmail.com>
2022-02-07 14:09:39 -0400

    feat: cria e visualiza projetos

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <title></title>
+</head>
+
+<body>
+    <h1>Birdboard</h1>
+    <ul>
+        @foreach ($projects as $project)
+            <li>
+                {{ $project->title }}
+            </li>
+        @endforeach
+    </ul>
+</body>
+
+</html>
