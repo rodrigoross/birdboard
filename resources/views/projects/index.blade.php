352a99b7491e8d482362fedcc7f8a091eb5b0cf3
rodrigoross <rodrigo.ross.comp@gmail.com>
2022-02-08 15:28:37 -0400

    refactor: altera layout de projetos pessoais

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,26 +1,24 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Birdboard') }}
-        </h2>
+        <div class="flex justify-between align-middle">
+            <h1 class="font-bold text-sm text-gray-500 uppercase self-center">{{ __('Meus projetos') }}</h1>
+
+            <x-button class="shadow shadow-sm">
+                <a href="{{ route('projects.create') }}">{{ __('Novo projeto') }}</a>
+            </x-button>
+        </div>
     </x-slot>
 
-    <div class="flex justify-between align-middle mb-5">
-        <h1 class="font-bold text-sm text-gray-500 uppercase self-center">Meus projetos</h1>
 
-        <x-button class="">
-            <a href="{{ route('projects.create') }}">Novo projeto</a>
-        </x-button>
-    </div>
-
-    <div class="grid grid-cols-3 gap-3">
+    <div class="grid lg:grid-cols-3 md:grid-cols-2 gap-3">
         @forelse ($projects as $project)
-            <div class="bg-white rounded shadow shadow-sm p-5" style="height: 200px">
-                <h3 class="font-normal text-xl py-4">
-                    {{ $project->title }}
+            <div class="bg-white rounded-lg shadow shadow-sm p-5" style="height: 200px">
+                <h3
+                    class="-ml-5 font-normal text-xl py-4 pl-5 mb-3 border-solid border-l-4 border-l-sky-500 hover:text-sky-500 transition ease-in-out">
+                    <a href="{{ route('projects.show', $project->id) }}">{{ $project->title }}</a>
                 </h3>
 
-                <div class="text-gray-500">
+                <div class="text-gray-500 md:truncate">
                     {{ Str::limit($project->description, 180, '...') }}
                 </div>
             </div>
@@ -28,15 +26,4 @@
             <div>{{ __('Você não tem nenhum projeto ainda!') }}</div>
         @endforelse
     </div>
-
-    {{-- <ul class="list-inside">
-        @forelse  ($projects as $project)
-            <li
-                class="my-1 p-2 bg-white rounded shadow shadow-sm hover:bg-gray-50 hover:text-indigo-700 hover:shadow-md">
-                <a href="{{ $project->path() }}">{{ $project->title }}</a>
-            </li>
-        @empty
-            <li>{{ __('Nenhun projeto criado!') }}</li>
-        @endforelse
-    </ul> --}}
 </x-app-layout>
