461fed043fe5a2b386f796592253f9a7f5cc5e30
rodrigoross <rodrigo.ross.comp@gmail.com>
2022-02-15 16:14:45 -0400

    refactor: aplica validação e autorizacao

--- app/Http/Controllers/ProjectInvitationsController.php
+++ app/Http/Controllers/ProjectInvitationsController.php
@@ -13,8 +13,18 @@ class ProjectInvitationsController extends Controller
      */
     public function store(Project $project)
     {
+        $this->authorize('update', $project);
+
+        request()->validate([
+            'email' => 'required|exists:users,email'
+        ], [
+            'email.exists' => 'O usuário que está sendo convidado deve uma conta no Birdboard'
+        ]);
+
         $user = User::whereEmail(request('email'))->first();
 
         $project->invite($user);
+
+        return redirect($project->path());
     }
 }
